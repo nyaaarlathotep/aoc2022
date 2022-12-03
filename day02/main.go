51c3381e166e0e476cd9ba10bd22f2848bf83995
nyaaar-ubuntu <nyaaarlathotep@163.com>
2022-12-03 19:10:37 +0800

    feat day02

--- day02/main.go
+++ day02/main.go
@@ -3,67 +3,172 @@ package main
 import (
 	"aoc2022/util"
 	"log"
-	"strconv"
+)
+
+type elfMove string
+type myMove string
+
+const (
+	Rock       elfMove = "A"
+	Paper      elfMove = "B"
+	Scissors   elfMove = "C"
+	MyRock     myMove  = "X"
+	MyPaper    myMove  = "Y"
+	MyScissors myMove  = "Z"
 )
 
 func main() {
 	input := util.GetInput("02")
-	lines := util.GetStringSlice(input, "\n")
-	cals := make([][]int, 1)
-	index := 0
-	for _, line := range lines {
-		if line == "" {
-			index++
-			cals = append(cals, make([]int, 0))
-			continue
-		}
-		calNum, err := strconv.Atoi(line)
-		if err != nil {
-			log.Fatalln(err)
-
-		}
-		cals[index] = append(cals[index], calNum)
-	}
-	calSum := make([]int, len(cals))
-	for i, cal := range cals {
-		total := 0
-		for _, num := range cal {
-			total += num
-		}
-		calSum[i] = total
-	}
-	partOne(calSum)
-
-	maxOne := 0
-	maxTwo := 0
-	maxThree := 0
-	for _, calEach := range calSum {
-		if calEach > maxOne {
-			maxThree = maxTwo
-			maxTwo = maxOne
-			maxOne = calEach
-			continue
-		}
-		if calEach > maxTwo {
-			maxThree = maxTwo
-			maxTwo = calEach
-			continue
-		}
-		if calEach > maxThree {
-			maxThree = calEach
-			continue
-		}
-	}
-	log.Printf("%+v", maxTwo+maxOne+maxThree)
+	rawMoves := util.Get2dString(input, "\n", " ")
+	partOne(rawMoves)
+	partTwo(rawMoves)
 }
 
-func partOne(calSum []int) {
-	max := 0
-	for _, calEach := range calSum {
-		if calEach > max {
-			max = calEach
-
-		}
+func partOne(rawMoves [][]string) {
+	total := 0
+	for _, rawMove := range rawMoves {
+		elf := parseElfMove(rawMove[0])
+		my := parseMyMove(rawMove[1])
+		winScore := my.win(elf)
+		total = total + winScore + my.moveScore()
 	}
-	log.Printf("%+v", max)
+	log.Printf("%+v", total)
+}
+
+func partTwo(rawMoves [][]string) {
+	total := 0
+	for _, rawMove := range rawMoves {
+		elf := parseElfMove(rawMove[0])
+		winScore := scorePartTwo(rawMove[1], elf)
+		total = total + winScore
+	}
+	log.Printf("%+v", total)
+}
+
+func parseElfMove(move string) elfMove {
+	if move == "A" {
+		return Rock
+	}
+	if move == "B" {
+		return Paper
+	}
+	if move == "C" {
+		return Scissors
+	}
+	log.Fatalln("???")
+	return Rock
+}
+
+func parseMyMove(move string) myMove {
+	if move == "X" {
+		return MyRock
+	}
+	if move == "Y" {
+		return MyPaper
+	}
+	if move == "Z" {
+		return MyScissors
+	}
+	log.Fatalln("???")
+	return MyRock
+}
+func scorePartTwo(move string, move2 elfMove) int {
+	if move == "X" {
+		if move2 == Rock {
+			return 3
+		}
+		if move2 == Scissors {
+			return 2
+		}
+		if move2 == Paper {
+			return 1
+		}
+		log.Fatalln("???")
+		return 0
+	}
+	if move == "Y" {
+		if move2 == Rock {
+			return 4
+		}
+		if move2 == Scissors {
+			return 6
+		}
+		if move2 == Paper {
+			return 5
+		}
+		log.Fatalln("???")
+		return 0
+	}
+	if move == "Z" {
+		if move2 == Rock {
+			return 8
+		}
+		if move2 == Scissors {
+			return 7
+		}
+		if move2 == Paper {
+			return 9
+		}
+		log.Fatalln("???")
+		return 0
+	}
+	panic("scorePartTwo")
+}
+
+func (my *myMove) win(move elfMove) int {
+	if *my == MyRock {
+		if move == Rock {
+			return 3
+		}
+		if move == Scissors {
+			return 6
+		}
+		if move == Paper {
+			return 0
+		}
+		log.Fatalln("???")
+		return 0
+	}
+
+	if *my == MyPaper {
+		if move == Rock {
+			return 6
+		}
+		if move == Scissors {
+			return 0
+		}
+		if move == Paper {
+			return 3
+		}
+		log.Fatalln("???")
+		return 0
+	}
+	if *my == MyScissors {
+		if move == Rock {
+			return 0
+		}
+		if move == Scissors {
+			return 3
+		}
+		if move == Paper {
+			return 6
+		}
+		log.Fatalln("???")
+		return 0
+	}
+	log.Fatalln("???")
+	return 0
+}
+
+func (my *myMove) moveScore() int {
+	if *my == MyRock {
+		return 1
+	}
+	if *my == MyPaper {
+		return 2
+	}
+	if *my == MyScissors {
+		return 3
+	}
+	panic("moveScore")
 }
