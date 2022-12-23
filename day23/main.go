fe5a7e41589948063f666533af25b73d85ecd82a
nyaaar-ubuntu <nyaaarlathotep@163.com>
2022-12-23 21:00:59 +0800

    feat day23 partTwo

--- day23/main.go
+++ day23/main.go
@@ -11,11 +11,20 @@ import (
 
 const elfPlaceHolder = '#'
 const blank = 0
+const margin = 100
 
 func main() {
 	start := time.Now()
 	input := util.GetInput("23")
 	lines := util.GetStringSlice(input, "\n")
+
+	partOne(lines)
+	partTwo(lines)
+	elapsed := time.Now().Sub(start)
+	fmt.Println("该函数执行完成耗时：", elapsed)
+}
+
+func partOne(lines []string) {
 	g := DataStruct.NewGrid[rune](int64(len(lines[0]))+20, int64(len(lines))+20, DataStruct.Directions8)
 
 	elves := make([]DataStruct.Point, 0)
@@ -92,8 +101,71 @@ func main() {
 	}
 
 	fmt.Printf("%v\n", (maxX-minX+1)*(maxY-minY+1)-int64(len(elves)))
-	elapsed := time.Now().Sub(start)
-	fmt.Println("该函数执行完成耗时：", elapsed)
+}
+
+func partTwo(lines []string) {
+	g := DataStruct.NewGrid[rune](int64(len(lines[0]))+2*margin, int64(len(lines))+2*margin, DataStruct.Directions8)
+
+	elves := make([]DataStruct.Point, 0)
+
+	for i, l := range lines {
+		y := i + margin
+		for j, r := range l {
+			x := j + margin
+			if r == '.' {
+				r = blank
+			}
+			g.SetState(int64(x), int64(y), r)
+			if r == elfPlaceHolder {
+				elves = append(elves, DataStruct.Point{
+					X: int64(x),
+					Y: int64(y),
+				})
+			}
+		}
+	}
+	dirs := []func(elf DataStruct.Point, g *DataStruct.Grid[rune]) (DataStruct.Point, bool){goNorth, goSouth, goWest, goEast}
+	dirIndex := 0
+	//printG(g)
+	rounds := 0
+	for ; ; rounds++ {
+		dest := make(map[DataStruct.Point]DataStruct.Point)
+		for _, elf := range elves {
+			neighbours := g.Neighbours(elf)
+			move := false
+			for _, p := range neighbours {
+				r := g.GetPointState(p)
+				if r == elfPlaceHolder {
+					move = true
+					break
+				}
+			}
+			if !move {
+				continue
+			}
+
+			p := getNextLegalP(elf, g, dirIndex, dirs)
+			//fmt.Printf("%+v -> %+v\n", elf, p)
+			if _, ok := dest[p]; !ok {
+				dest[p] = elf
+			} else {
+				//fmt.Printf("duplicate: %+v\n", p)
+				delete(dest, p)
+			}
+		}
+		if len(dest) == 0 {
+			rounds++
+			break
+		}
+		for p, elf := range dest {
+			g.SetState(elf.X, elf.Y, '.')
+			g.SetState(p.X, p.Y, elfPlaceHolder)
+		}
+		elves = maps.Keys(g.StateMapWhere(func(r rune) bool { return r == elfPlaceHolder }))
+		//printG(g)
+		dirIndex++
+	}
+	fmt.Printf("%v\n", rounds)
 }
 
 func getNextLegalP(elf DataStruct.Point, g *DataStruct.Grid[rune], dirIndex int,
